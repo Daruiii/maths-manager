ff634865e645e8e08abf040875aea8229e8c5843
David Meguira <davidmgr93@gmail.com>
2024-03-13 20:04:11 +0100

    fix social connection

--- mathsManager/app/Http/Controllers/Auth/ProviderController.php
+++ mathsManager/app/Http/Controllers/Auth/ProviderController.php
@@ -12,20 +12,30 @@ class ProviderController extends Controller
 {
     public function redirect($provider)
     {
-        return Socialite::driver($provider)->redirect();
+        return Socialite::driver($provider)
+                    ->with(['prompt' => 'select_account'])
+                    ->redirect();
     }
 
     public function callback($provider)
-    {
-        try {
-            $SocialUser = Socialite::driver($provider)->user();
-            if (User::where('email', $SocialUser->email)->exists()) {
-                return redirect('/login')->withErrors(['email' => 'This email uses different method to login']);
+{
+    try {
+        $SocialUser = Socialite::driver($provider)->user();
+
+        // Recherchez d'abord un utilisateur avec le même provider et provider_id.
+        $user = User::where('provider', $provider)
+                    ->where('provider_id', $SocialUser->id)
+                    ->first();
+
+        // Si aucun utilisateur n'est trouvé par provider et provider_id, vérifiez par email.
+        if (!$user) {
+            $user = User::where('email', $SocialUser->email)->first();
+            // Si un utilisateur est trouvé par email mais a utilisé une méthode différente pour s'inscrire, renvoyez une erreur.
+            if ($user && ($user->provider !== $provider)) {
+                return redirect('/login')->withErrors(['email' => 'This email uses different method to login.']);
             }
-            $user = User::where([
-                'provider' => $provider,
-                'provider_id' => $SocialUser->id
-            ])->first();
+
+            // Si aucun utilisateur n'est trouvé par email, créez un nouveau compte.
             if (!$user) {
                 $user = User::create([
                     'name' => $SocialUser->getName(),
@@ -36,10 +46,13 @@ class ProviderController extends Controller
                     'email_verified_at' => now()
                 ]);
             }
-            Auth::login($user);
-            return redirect('/home');
-        } catch (\Exception $e) {
-            return redirect('/login');
         }
+
+        // Connectez l'utilisateur
+        Auth::login($user);
+        return redirect('/home');
+    } catch (\Exception $e) {
+        return redirect('/login');
     }
+}
 }
