5019202fa07dfc604d1f78de341f524e8d3c5888
David Meguira <davidmgr93@gmail.com>
2024-04-15 18:36:09 +0200

    update backup

--- backup.sql
+++ backup.sql
@@ -3,6 +3,7 @@
 -- Host: localhost    Database: mathsManager
 -- ------------------------------------------------------
 -- Server version	10.5.21-MariaDB-0+deb11u1
+-- test
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
