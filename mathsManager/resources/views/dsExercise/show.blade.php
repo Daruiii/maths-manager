ad0c63a550d89f8eb457b88dc7f38ce345147b91
David Meguira <davidmgr93@gmail.com>
2024-04-29 18:08:47 +0200

    changement sens fleche ds exo

--- mathsManager/resources/views/dsExercise/show.blade.php
+++ mathsManager/resources/views/dsExercise/show.blade.php
@@ -8,25 +8,7 @@
     @if ($previousExercise)
         <a href="{{ route('ds_exercise.show', ['id' => $previousExercise->id, 'filter' => $filter ]) }}"
             class="previous-btn">
-            <svg width="16px" height="16px" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" fill="#000000">
-                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
-                <g id="SVGRepo_iconCarrier">
-                    <path fill="#000000"
-                        d="M224 480h640a32 32 0 1 1 0 64H224a32 32 0 0 1 0-64z">
-                    </path>
-                    <path fill="#000000"
-                        d="m237.248 512 265.408 265.344a32 32 0 0 1-45.312 45.312l-288-288a32 32 0 0 1 0-45.312l288-288a32 32 0 1 1 45.312 45.312L237.248 512z">
-                    </path>
-                </g>
-            </svg>
             <span>Exercice précédent</span>
-        </a>
-    @endif
-    @if ($nextExercise)
-        <a href="{{ route('ds_exercise.show', ['id' => $nextExercise->id, 'filter' => $filter ]) }}"
-            class="next-btn">
-            <span>Exercice suivant</span>
             <svg width="16px" height="16px" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" fill="#000000">
                 <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
                 <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
@@ -39,6 +21,24 @@
                     </path>
                 </g>
             </svg>
+        </a>
+    @endif
+    @if ($nextExercise)
+        <a href="{{ route('ds_exercise.show', ['id' => $nextExercise->id, 'filter' => $filter ]) }}"
+            class="next-btn">
+            <svg width="16px" height="16px" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" fill="#000000">
+                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                <g id="SVGRepo_iconCarrier">
+                    <path fill="#000000"
+                        d="M224 480h640a32 32 0 1 1 0 64H224a32 32 0 0 1 0-64z">
+                    </path>
+                    <path fill="#000000"
+                        d="m237.248 512 265.408 265.344a32 32 0 0 1-45.312 45.312l-288-288a32 32 0 0 1 0-45.312l288-288a32 32 0 1 1 45.312 45.312L237.248 512z">
+                    </path>
+                </g>
+            </svg>
+            <span>Exercice suivant</span>
         </a>
     @endif
     </div>
