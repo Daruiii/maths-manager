250753657a620eee111323131d3aac36a7349986
David Meguira <davidmgr93@gmail.com>
2024-11-15 04:03:01 +0100

    régler prblm syntaxe

--- mathsManager/resources/views/components/homeGuest.blade.php
+++ mathsManager/resources/views/components/homeGuest.blade.php
@@ -7,9 +7,9 @@
         
         <!-- Bloc de Bienvenue à gauche -->
         <div class="flex flex-col w-full lg:w-2/3 bg-white p-6 rounded-lg shadow-md">
-            <h2 class="text-xl lg:text-2xl font-bold text-blue-700 text-center lg:text-left">{{!! $introTitle ?? 'Bienvenue sur Maths Manager'!!}}</h2>
+            <h2 class="text-xl lg:text-2xl font-bold text-blue-700 text-center lg:text-left">{!! $introTitle ?? 'Bienvenue sur Maths Manager'!!}</h2>
             <p class="mt-4 text-gray-700 text-sm lg:text-base leading-relaxed">
-                {{!! $introContent ?? 'Vous trouverez sur ce site des exercices, des quizz, des fiches récapitulatives de cours sur tous les chapitres des classes de Première et Terminale. Les exercices ne disposant pas de correction, vous aurez la possibilité d’envoyer votre travail afin d’obtenir une correction de ma part. Les quizz sont interactifs et permettent de vérifier que le cours est su. Vous pourrez suivre votre progression durant l’année grâce au système de notation et de suivi des exercices et quizz. Un générateur de DS vous permet de concevoir de manière automatique et aléatoire un contrôle personnalisé en fonction de la difficulté, du temps et des chapitres sélectionnés.' !!}}
+                {!! $introContent ?? 'Vous trouverez sur ce site des exercices, des quizz, des fiches récapitulatives de cours sur tous les chapitres des classes de Première et Terminale. Les exercices ne disposant pas de correction, vous aurez la possibilité d’envoyer votre travail afin d’obtenir une correction de ma part. Les quizz sont interactifs et permettent de vérifier que le cours est su. Vous pourrez suivre votre progression durant l’année grâce au système de notation et de suivi des exercices et quizz. Un générateur de DS vous permet de concevoir de manière automatique et aléatoire un contrôle personnalisé en fonction de la difficulté, du temps et des chapitres sélectionnés.' !!}
             </p>
             <p class="mt-4 text-sm">
                 <a href="{{ route('login') }}" class="text-indigo-500 hover:text-indigo-700 font-semibold underline">Connectez-vous</a> et contactez
@@ -24,12 +24,12 @@
 
         <!-- Bloc "Qui suis-je ?" à droite avec hauteur fixe et barre de défilement -->
         <div class="flex flex-col items-center w-full lg:w-1/3 bg-white p-6 rounded-lg shadow-md mt-10 lg:mt-0 max-h-fit overflow-y-auto">
-            <h2 class="text-xl font-bold text-blue-700 text-center">{{!! $whoamiTitle ?? 'Qui suis-je ?' !!}}</h2>
+            <h2 class="text-xl font-bold text-blue-700 text-center">{!! $whoamiTitle ?? 'Qui suis-je ?' !!}</h2>
             @if ($whoamiImage)
                 <img src="{{ asset($whoamiImage) }}" alt="Photo de Maxime" class="w-24 h-24 rounded-full my-4 shadow-md">
             @endif
             <p class="text-sm text-gray-600 text-center mt-4">
-                {{!! $whoamiContent ?? 'Après deux années de classes préparatoires MPSI, MP*, j’ai intégré l’école d’ingénieur ENSEEIHT. Je suis professeur particulier depuis maintenant 8 années, où j’ai pu aider de nombreux élèves à obtenir leur Baccalauréat et poursuivre leurs études dans le supérieur.' !!}}
+                {!! $whoamiContent ?? 'Après deux années de classes préparatoires MPSI, MP*, j’ai intégré l’école d’ingénieur ENSEEIHT. Je suis professeur particulier depuis maintenant 8 années, où j’ai pu aider de nombreux élèves à obtenir leur Baccalauréat et poursuivre leurs études dans le supérieur.' !!}
             </p>
         </div>
     </div>
